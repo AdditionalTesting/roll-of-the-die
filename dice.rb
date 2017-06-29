8d733fe62bbdf5f56e8dec9450e4cc8d31ea3bfb
AlB <albert.burc@gmail.com>
2017-06-29 18:48:10 -0400

    finishd dice part

---
+++ dice.rb
@@ -0,0 +1,5 @@
+die_one = Random.rand(6) + 1
+die_two = Random.rand(6) + 1
+
+puts "You rolled #{die_one} and #{die_two}"
+puts "Your total is #{die_one + die_two}"
--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,5 +1,5 @@
 array = []
-10.times do
+5.times do
   die_one = Random.rand(6) + 1
   array << die_one
 end
@@ -9,3 +9,6 @@ array.sort!
 array.each do |num|
   puts "The result of your roll is #{num}"
 end
+
+puts "The lowest number is #{array.first}"
+puts "The highest number is #{array.last}"
