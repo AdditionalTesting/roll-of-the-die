c78897722eb49e4595949f1eab92886aac76980a
AlB <albert.burc@gmail.com>
2017-06-29 19:20:09 -0400

    finished permutation task

---
+++ doubles.rb
@@ -0,0 +1,10 @@
+die_one = Random.rand(6) + 1
+die_two = Random.rand(6) + 1
+
+puts "You rolled #{die_one} and #{die_two}"
+
+if die_one == die_two
+  puts "Doubles!"
+end
+
+puts "Your total is #{die_one + die_two}"
---
+++ permutations.rb
@@ -0,0 +1,5 @@
+(1..6).each do |num1|
+  (1..6).each do |num2|
+  puts "This is it #{num1} And this is #{num2}"
+end
+end
